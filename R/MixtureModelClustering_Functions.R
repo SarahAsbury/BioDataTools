6d0e1f5e5df83a905686e7940c16446dda82b524
SarahAsbury <83042411+SarahAsbury@users.noreply.github.com>
2022-06-16 16:52:42 -0400

    Fix logr

--- R/MixtureModelClustering_Functions.R
+++ R/MixtureModelClustering_Functions.R
@@ -137,7 +137,7 @@ bootstrap.rmixmod <- function(df,
   print("Register parallel cores")
 
   #Start log
-  log_open(file_name = "ClusterStabilityLog.txt")
+  log_open()
   print("Log open")
   log_print(paste("Export results to:", directory))
 
